7683cf864a26b51da5c402ab2cdd1027d3c16291
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-10-11 09:18:43 +0300

    feat: create a separate function getPlaceholderText, updated vars, refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,35 @@
 'use strict';
 
+function getPlaceholderText(fieldName) {
+  const firstUpperIndex = fieldName
+    .split('')
+    .findIndex((ch) => ch === ch.toUpperCase());
+  const spaceInsertIndex = firstUpperIndex;
+
+  if (firstUpperIndex > 0) {
+    return (
+      fieldName[0].toUpperCase() +
+      fieldName.slice(1, spaceInsertIndex) +
+      ' ' +
+      fieldName.slice(spaceInsertIndex)
+    );
+  } else {
+    return fieldName[0].toUpperCase() + fieldName.slice(1);
+  }
+}
+
 const fields = [...document.querySelectorAll('input.field-text')];
 
 fields.forEach((field) => {
   const label = document.createElement('label');
   const fieldName = field.name;
 
-  const index = fieldName.split('').findIndex((ch) => ch === ch.toUpperCase());
-  const indexEnd = index;
-
-  const placeholderText =
-    index > 0
-      ? field.name[0].toUpperCase() +
-        field.name.slice(1, indexEnd) +
-        ' ' +
-        field.name.slice(indexEnd)
-      : field.name[0].toUpperCase() + field.name.slice(1);
+  const placeholderText = getPlaceholderText(fieldName);
 
   label.classList.add('field-label');
   label.setAttribute('for', field.id);
   label.textContent = placeholderText;
+
   field.setAttribute('placeholder', placeholderText);
   field.before(label);
 });
